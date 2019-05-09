f017c891cbde59fccab3e441f4d7ab8e0fad8e6e
Paul CACHEUX <paulcacheux@gmail.com>
2019-05-09 17:27:26 +0200

    Réu du 9 mai 2019

--- ctw.py
+++ ctw.py
@@ -1,110 +1,100 @@
-import random
-import sys
-import graphviz
-import markov
 from fractions import Fraction
+import markov
 
-def product(iterator):
-    res = 1
-    for i in iterator:
-        res *= i
-    return res
+class Tree:
+    def __init__(self, m):
+        self.m = m
+        self.top = Node(None, 0, None, [], m)
+        self.nodes = [self.top]
+    
+    def insert_node(self, value, depth, parent):
+        node = Node(value, depth, parent, [], m)
+        self.nodes.append(node)
+        return node
+    
+    def get_node_of_depth(self, depth):
+        return [node for node in self.nodes if node.depth == depth]
 
+    def debug_print(self):
+        for node in self.nodes:
+            print("{}".format(repr(node)))
+        
+    def compute_prob(self, beta):
+        for node in self.nodes:
+            node.compute_pe()
+        self.top.compute_pw(beta)
+    
 class Node:
-    def __init__(self, N, value=None):
-        self.N = N
+    def __init__(self, value, depth, parent, children, m):
+        self.m = m
         self.value = value
-        self.count = [0] * N
-        self.children = [None] * N
-
-    def is_leaf(self):
-        return all(c is None for c in self.children)
-
-    def add_suffix(self, suffix, then):
-        if len(suffix) == 0:
-            return
-
-        last = suffix[-1]
-        rest = suffix[:-1]
-
-        if self.children[last] == None:
-            self.children[last] = Node(self.N, last)
-
-        self.children[last].count[then] += 1
-        self.children[last].add_suffix(rest, then)
-
-    def pretty_print(self, depth=0):
-        tab = "\t" * depth
-        print(tab + "Node(value={}, count={}, pe={}, pw={}, pm={})".format(self.value, self.count, self.pe, self.pw, self.pm))
-        for c in self.children:
-            if c != None:
-                c.pretty_print(depth + 1)
+        self.depth = depth
+        self.parent = parent
+        self.children = children
+        self.count = [0] * m
+        self.pe = None
+    
+    def __repr__(self):
+        return "Node(depth={}, value={}, context={}, count={}, pe={}, pw={})".format(self.depth, self.value, self.get_context(), self.count, float(self.pe), float(self.pw))
+    
+    def get_context(self):
+        if self.parent is None:
+            return []
+        return [self.value] + self.parent.get_context()
     
     def compute_pe(self):
-        res = product(i + Fraction(1, 2) for j in range(self.N) for i in range(self.count[j]))
-        M = sum(self.count)
-        for i in range(M):
-            coeff = Fraction(self.N, 2) + i
-            res /= coeff
+        Ms = sum(self.count)
+        num = 1
+        for j in range(self.m):
+            for i in range(self.count[j]):
+                num *= Fraction(1, 2) + i
+        
+        den = 1
+        for i in range(Ms):
+            den *= Fraction(m, 2) + i
+        
+        res = num / den
         self.pe = res
     
     def compute_pw(self, beta):
-        if self.is_leaf():
+        if len(self.children) == 0:
             self.pw = self.pe
         else:
-            prod = product(c.pw for c in self.children if c is not None)
-            self.pw = beta * self.pe + (1 - beta) * prod
-    
-    def compute_pm(self, beta):
-        if self.is_leaf():
-            self.pm = self.pe
-        else:
-            prod = product(c.pm for c in self.children if c is not None)
-            inv_beta_prod = (1 - beta) * prod
-            self.pm = max(beta * self.pe, inv_beta_prod)
-
-
-    def compute_proba(self, beta):
-        for c in self.children:
-            if c is not None:
-                c.compute_proba(beta)
-        
-        self.compute_pe()
-        self.compute_pw(beta)
-        self.compute_pm(beta)
-    
-    def prune(self, beta):
-        if self.pm == beta * self.pe:
-            self.children = [None] * self.N
-        else:
+            p = 1
             for c in self.children:
-                if c is not None:
-                    c.prune(beta)
+                c.compute_pw(beta)
+                p *= c.pw
+            self.pw = beta * self.pe + (1 - beta) * p
 
+    
+def build_tree(tree, data, D):
+    for i in range(1, D+1):
+        for j in range(len(data) - i + 1):
+            context = data[j:j+i]
+            value = context[0]
+            rest = context[1:]
+            depth = len(rest)
+            after = None
+            if j+i < len(data):
+                after = data[j+i]
 
-def build_ctw(input_bytes, mem_size, N):
-    node = Node(N)
-    suffix_size = mem_size + 1
-    for i in range(len(input_bytes) - suffix_size + 1):
-        suffix, then = input_bytes[i:i+mem_size], input_bytes[i + mem_size]
-        # print("adding : ({} -> {})".format(suffix, then))
-        node.add_suffix(suffix, then)
-    return node
+            for node in tree.get_node_of_depth(depth):
+                if node.get_context() == rest:
+                    value_node = get_node_in_tree(tree, node, depth, value)
+                    if after is not None:
+                        value_node.count[after] += 1
 
-def main():
-    # input_bits = [0, 1, 2, 3, 2, 1] * 3
-    # input_bits = [random.randrange(4) for _ in range(40)]
-    input_bits = markov.gen_markov(10000)
-    # input_bits = [0, 0] + [1, 1, 0, 0, 1, 0, 1, 0, 1, 0]
-    # input_bits = [0, 0] + [0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1]
-    # print(input_bits)
-    node = build_ctw(input_bits, 4, 2)
-    beta = 0.5
-    node.compute_proba(beta)
-    # node.prune(beta)
-    # Node.pretty_print(node)
-    print(graphviz.main_node_to_graphviz(node))
+def get_node_in_tree(tree, current_node, depth, value):
+    for c in current_node.children:
+        if c.value == value:
+            return c
+    c = tree.insert_node(value, depth + 1, current_node)
+    current_node.children.append(c)
+    return c
 
-if __name__ == "__main__":
-    main()
-
+m = 2
+tree = Tree(m)
+input_bits = markov.gen_markov(100)
+build_tree(tree, input_bits, 3)
+tree.compute_prob(Fraction(1, 2))
+tree.debug_print()
---
+++ ctw_nearly.py
@@ -0,0 +1,117 @@
+import random
+import sys
+import graphviz
+import markov
+from fractions import Fraction
+
+def product(iterator):
+    res = 1
+    for i in iterator:
+        res *= i
+    return res
+
+class Node:
+    def __init__(self, N, value=None):
+        self.N = N
+        self.value = value
+        self.count = [0] * N
+        self.children = [None] * N
+
+    def is_leaf(self):
+        return all(c is None for c in self.children)
+
+    def add_suffix(self, suffix, then):
+        if len(suffix) == 0:
+            return
+
+        last = suffix[-1]
+        rest = suffix[:-1]
+
+        if self.children[last] == None:
+            self.children[last] = Node(self.N, last)
+
+        self.children[last].count[then] += 1
+        self.children[last].add_suffix(rest, then)
+
+    def pretty_print(self, depth=0):
+        tab = "\t" * depth
+        print(tab + "Node(value={}, count={}, pe={}, pw={}, pm={})".format(self.value, self.count, self.pe, self.pw, self.pm))
+        for c in self.children:
+            if c != None:
+                c.pretty_print(depth + 1)
+    
+    def compute_pe(self):
+        toppart = 1
+        for j in range(self.N):
+            for i in range(self.count[j]):
+                toppart *= (Fraction(1, 2) + i)
+        
+        bottompart = 1
+        M = sum(self.count)
+        for i in range(M):
+            coeff = Fraction(self.N, 2) + i
+            bottompart *= coeff
+
+        res = toppart / bottompart
+        self.pe = res
+    
+    def compute_pw(self, beta):
+        if self.is_leaf():
+            self.pw = self.pe
+        else:
+            prod = product(c.pw for c in self.children if c is not None)
+            self.pw = beta * self.pe + (1 - beta) * prod
+    
+    def compute_pm(self, beta):
+        if self.is_leaf():
+            self.pm = self.pe
+        else:
+            prod = product(c.pm for c in self.children if c is not None)
+            inv_beta_prod = (1 - beta) * prod
+            self.pm = max(beta * self.pe, inv_beta_prod)
+
+
+    def compute_proba(self, beta):
+        for c in self.children:
+            if c is not None:
+                c.compute_proba(beta)
+        
+        self.compute_pe()
+        self.compute_pw(beta)
+        self.compute_pm(beta)
+    
+    def prune(self, beta):
+        if self.pm == beta * self.pe:
+            self.children = [None] * self.N
+        else:
+            for c in self.children:
+                if c is not None:
+                    c.prune(beta)
+
+
+def build_ctw(input_bytes, mem_size, N):
+    node = Node(N)
+    suffix_size = mem_size + 1
+    for i in range(len(input_bytes) - suffix_size + 1):
+        suffix, then = input_bytes[i:i+mem_size], input_bytes[i + mem_size]
+        # print("adding : ({} -> {})".format(suffix, then))
+        node.add_suffix(suffix, then)
+    return node
+
+def main():
+    # input_bits = [0, 1, 2, 3, 2, 1] * 3
+    # input_bits = [random.randrange(4) for _ in range(40)]
+    input_bits = markov.gen_markov(10000)
+    # input_bits = [0, 0] + [1, 1, 0, 0, 1, 0, 1, 0, 1, 0]
+    # input_bits = [0, 0] + [0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1]
+    # print(input_bits)
+    node = build_ctw(input_bits, 4, 2)
+    beta = 0.5
+    node.compute_proba(beta)
+    node.prune(beta)
+    # Node.pretty_print(node)
+    print(graphviz.main_node_to_graphviz(node))
+
+if __name__ == "__main__":
+    main()
+
