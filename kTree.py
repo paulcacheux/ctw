5922c9fa7052497d7b9f5185e0b13e80fb4529cd
Paul CACHEUX <paulcacheux@gmail.com>
2019-05-24 12:13:09 +0200

    Add build matrix

--- kTree.py
+++ kTree.py
@@ -10,14 +10,29 @@ import markov
 import graphviz_ktree
 import numpy as np
 
+def product(iter):
+    res = 1
+    for elem in iter:
+        res *= elem
+    return res
+
+def ij_iterator(kj, m):
+    if m == 1:
+        for i in range(1, kj + 1):
+            yield (i,)
+    else:
+        for left in ij_iterator(kj, m - 1):
+            for i in range(1, kj + 1):
+                yield (*left, i)
+
 class Tree:
     def __init__(self, m, k):
         self.m = m
-        self.top = Node(None, 0, None, [], m)
+        self.top = Node(None, 0, None, [], m, k)
         self.nodes = [self.top]
     
     def insert_node(self, value, depth, parent, k):
-        node = Node(value, depth, parent, [], m, k)
+        node = Node(value, depth, parent, [], self.m, k)
         self.nodes.append(node)
         return node
     
@@ -68,19 +83,19 @@ class Node:
         
         den = 1
         for i in range(Ms):
-            den *= Fraction(m, 2) + i
+            den *= Fraction(self.m, 2) + i
         
         res = num / den
         self.pe = res
 
-def build_full_tree(tree, m, D):
+def build_full_tree(tree, m, D, k):
     for depth in range(0, D):
         for node in tree.get_node_of_depth(depth):
             for value in range(m):
-                value_node = get_node_in_tree(tree, node, depth, value)
+                value_node = get_node_in_tree(tree, node, depth, value, k)
 
     
-def build_tree(tree, data, D):
+def build_tree(tree, data, D, k):
     for i in range(1, D+1):
         for j in range(len(data) - i + 1):
             context = data[j:j+i]
@@ -93,15 +108,47 @@ def build_tree(tree, data, D):
 
             for node in tree.get_node_of_depth(depth):
                 if node.get_context() == rest:
-                    value_node = get_node_in_tree(tree, node, depth, value)
+                    value_node = get_node_in_tree(tree, node, depth, value, k)
                     if after is not None:
                         value_node.count[after] += 1
 
-def get_node_in_tree(tree, current_node, depth, value):
+def build_matrix(tree, D, beta):
+    m = tree.m
+    depth = D - 1
+    for node in tree.get_node_of_depth(depth):
+        node.pms[0] = node.pe
+        node.Bs[0] = np.zeros((1, m))
+
+    depth = D - 2
+    for node in tree.get_node_of_depth(depth):
+        left = beta * node.pe
+        right = (1 - beta) * product(c.pms[0] for c in node.children)
+        if left > right:
+            node.pms[0] = left
+            node.Bs[0] = np.zeros((1, m))
+            node.Bs[1] = np.ones((1, m))
+        else:
+            node.pms[1] = right
+            node.Bs[1] = np.zeros((1, m))
+            node.Bs[0] = np.ones((1, m))
+    for depth in range(3, D):
+        depth = D - depth
+        probas = [(beta * node.pe, np.zeros((1, m)))]
+        kj = D - depth
+        for ijs in ij_iterator(kj, m):
+            p = (1 - beta) * product(node.children[j].pms[ijs[j]] for j in range(m))
+            probas.append((p, ijs))
+        probas.sort(key=lambda p:p[0], reverse=True) # sort by proba in desc order
+        probas = probas[:k] # we keep only k of them
+        node.Bs = np.array(probas)
+        assert node.Bs.shape == (k, m)
+
+
+def get_node_in_tree(tree, current_node, depth, value, k):
     for c in current_node.children:
         if c.value == value:
             return c
-    c = tree.insert_node(value, depth + 1, current_node)
+    c = tree.insert_node(value, depth + 1, current_node, k)
     current_node.children.append(c)
     return c
 
@@ -112,15 +159,23 @@ def compute_input_proba(tree):
             proba*=node.pe
     return proba
 
-m = 3
-D = 2
-k = 3
-tree = Tree(m, k)
-#input_bits = markov.gen_markov(10)
-input_bits=[0,1,0,1,0,1,0,1,0,1,0,1,0,1]
-# print(input_bits)
-build_full_tree(tree, m, D)
-build_tree(tree, input_bits, D)
-tree.compute_prob()
-print(graphviz_ktree.main_node_to_graphviz(tree.top))
 
+def main():
+    m = 3
+    D = 2
+    k = 3
+    tree = Tree(m, k)
+    #input_bits = markov.gen_markov(10)
+    input_bits=[0,1,0,1,0,1,0,1,0,1,0,1,0,1]
+    # print(input_bits)
+    build_full_tree(tree, m, D, k)
+    build_tree(tree, input_bits, D, k)
+    tree.compute_prob()
+    build_matrix(tree, D, 0.5)
+    print(graphviz_ktree.main_node_to_graphviz(tree.top))
+
+def test():
+    for i in ij_iterator(2, 3):
+        print(i)
+
+main()
